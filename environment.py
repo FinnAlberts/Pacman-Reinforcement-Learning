a4316508c4cd3662788be87e20ad6555982988b4
Finn Alberts <finnalberts@gmail.com>
2022-02-23 14:09:02 +0100

    Add the basic structure toe Gym environment for Pacman

--- environment.py
+++ environment.py
@@ -11,5 +11,17 @@ class PacmanEnvironment(gym.env):
         # Create an observation space with values from 0 to 8 for different contents (walls, ghosts, Pacman, etc) in a 31 by 28 grid
         self.observation_space = spaces.Box(low=0, high=8, shape=(31, 28), dtype='uint8')
 
+    def step(self, action):
+        return
     
+    def _get_reward(self, gamestate: dict, controls: dict) -> float:
+        return
 
+    def _response_to_output(self, r: bytes, controls: dict):
+        return
+
+    def reset(self):
+        return
+
+    def render(self, mode='human', close=False):
+        return
