827fc3fcec78170eeda7d593275526d0c864ed92
Finn Alberts <finnalberts@gmail.com>
2022-03-08 14:20:25 +0100

    Disable logging of fruits

--- Pacman_Game/run.py
+++ Pacman_Game/run.py
@@ -192,7 +192,7 @@ class GameController(object):
         if self.pellets.numEaten == 50 or self.pellets.numEaten == 140:
             if self.fruit is None:
                 self.fruit = Fruit(self.nodes.getNodeFromTiles(9, 20), self.level)
-                print(self.fruit)
+                #print(self.fruit) # RLD - Disable logging of fruits
         if self.fruit is not None:
             if self.pacman.collideCheck(self.fruit):
                 self.updateScore(self.fruit.points)
