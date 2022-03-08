0b7638b1088b9131fb9868e2e0f0d180055a3fba
Finn Alberts <finnalberts@gmail.com>
2022-03-08 14:19:40 +0100

    Remove unused commented code

--- main.py
+++ main.py
@@ -4,11 +4,6 @@ import Pacman_Game.run
 from stable_baselines3 import PPO
 
 def main():
-    # game = Pacman_Game.run.GameController()
-    # game.startGame()
-    
-    # while True:
-    #     game.update()
     gym.register('Pacman-v0', entry_point=PacmanEnvironment)
     model = PPO('MlpPolicy', 'Pacman-v0').learn(10000000)
     
