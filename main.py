73d5ad2d1d3e0b58a719149ffd1bef36c7373f00
Finn Alberts <finnalberts@gmail.com>
2022-03-08 14:20:12 +0100

    Add saving and loading of the model
    By doing this, the model will continue training from the point where the application was closed previously

--- main.py
+++ main.py
@@ -2,12 +2,26 @@ import gym
 from environment import PacmanEnvironment
 import Pacman_Game.run
 from stable_baselines3 import PPO
+import os.path
+from os import path
 
 def main():
+    # Register Gym environment and create model
     gym.register('Pacman-v0', entry_point=PacmanEnvironment)
-    model = PPO('MlpPolicy', 'Pacman-v0').learn(10000000)
+    model = PPO('MlpPolicy', 'Pacman-v0')
+
+    # Check if a model already exists and if so load it
+    if path.exists("trained_model.zip"):
+        model.load("trained_model.zip")
+
+    # Run the program infinitely
+    while True:
+        # Learn for 20 000 steps
+        model.learn(20000)
+
+        # Save the model
+        print("Saving the model to trained_model.zip")
+        model.save("trained_model.zip")
     
-
-
 if __name__ == "__main__":
     main()
